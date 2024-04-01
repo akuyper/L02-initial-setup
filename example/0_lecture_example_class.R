44e9bc3049290a88b6bd5f0f2967dd24182d3d9f
akuyper <a-kuyper@northwestern.edu>
2024-04-01 14:59:25 -0500

    template update

---
+++ example/0_lecture_example_class.R
@@ -0,0 +1,11 @@
+# L02 Initial Setup ----
+# Lecture Example Classification
+# Target/Outcome Imbalance 
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
---
+++ example/0_lecture_example_reg.R
@@ -0,0 +1,11 @@
+# L02 Initial Setup ----
+# Lecture Example Regression
+# Target/Outcome Symmetry Issues 
+
+# load packages ----
+library(tidyverse)
+library(tidymodels)
+library(here)
+
+# handle common conflicts
+tidymodels_prefer()
